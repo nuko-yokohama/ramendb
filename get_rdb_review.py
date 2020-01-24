29e965b2d833fbc21cc4d126218201d36482999a
nuko-yokohama <nuko.yokohama@gmail.com>
2020-01-24 20:54:16 +0000

    レビュー収集スクリプト名の修正

--- get_rdb_review.py
+++ get_rdb_review.py
@@ -21,11 +21,37 @@ def url2uid(url):
 def url2sid(url):
 	return url.replace('/s/','').replace('.html', '')
 
+#
+# getCategory(t)
+#
+def getCategory(t):
+	if t.find('/') != -1 :
+		category = 'ラーメン'
+	elif t == 'カレーライス' or t == '欧風カレー' or t == 'インドカレー' or t == 'タイカレー' or \
+	     t == 'スープカレー' or t == 'ドライカレー' or t == '焼きカレー・カレードリア' or t == 'カレーパスタ' :
+		category = 'カレー'
+	elif t == 'チャーハン' or t == 'あんかけチャーハン' :
+		category = 'チャーハン'
+	elif t == '焼き餃子' or t == '水餃子' or t == '揚げ餃子' :
+		category = 'ぎょうざ'
+	elif t == 'かけうどん' or t == 'つけうどん' or t == '釜揚げうどん' or \
+	     t == '鍋焼きうどん' or t == '味噌煮込みうどん' or t == 'カレーうどん' or t == '焼きうどん' :
+		category = 'うどん'
+	elif t == '冷たいおそば' or t == '温かいおそば' :
+		category = 'そば'
+	else :
+		category = 'その他'
+	return category
+	
 
 def printReview(num):
-	url = 'https://ramendb.supleks.jp/review/' + str(num) + '.html'
+	url = 'https://supleks.jp/review/' + str(num) + '.html'
 	try:
 		res = req.urlopen(url)
+		if res is None :
+			print("urlopen error.")
+			retrun
+
 		soup = BeautifulSoup(res, "html.parser")
 		if soup is None :
 			print ("BeautifulSoup error.")
@@ -38,24 +64,36 @@ def printReview(num):
 		else :
 			menu =menuNode.string
 			menu = menu.replace(chr(165), '￥').replace('\\', '￥').replace('&','＆')
+			menu = menu.replace('	', ' ')
 
 		# score
 		scoreNode = soup.find('div', class_="score")
 		if scoreNode is None :
 			score = -1
-		else :
-			score = scoreNode.text.replace('点','')
-			score = scoreNode.text.replace('未満','')
+
+		score = scoreNode.text.replace('点','')
+		score = score.replace('未満','')
+		if score == '-' :
+			# null data
+			score = '\\N'
+		
+		# category
+		styleNode = soup.find('span', class_="style")
+		t = styleNode.text.replace('[','').replace(']','')
+		category = getCategory( t )
 
 		# noodle_type, soup_type
-		styleNode = soup.find('span', class_="style")
-		styleArray = styleNode.text.replace('[','').replace(']','').split('/')
+		if category == 'ラーメン' :
+			styleArray = t.split('/')
 			
-		if len(styleArray) == 2 :
-			noodle_type = styleArray[0]
-			soup_type = styleArray[1]
+			if len(styleArray) == 2 :
+				_type = styleArray[0]
+				soup_type = styleArray[1]
+			else :
+				return
 		else :
-			return
+			_type = t
+			soup_type = ''
 
 		# div props
 		divPropse = soup.find('div', class_="props")
@@ -85,14 +123,14 @@ def printReview(num):
 		review_timestamp = soup.find('time').attrs['datetime']
 
 		# print review information
-		rv = format(num) + '\t' + menu + '\t' + score + '\t' + noodle_type + '\t' + soup_type + '\t' + shop_id + '\t' + user_id + '\t' + likes + '\t' + review_timestamp[:10]
+		rv = format(num) + '\t' + menu + '\t' + score + '\t' + category + '\t' + _type + '\t' + soup_type + '\t' + shop_id + '\t' + user_id + '\t' + likes + '\t' + review_timestamp[:10]
 
 		try:
 			print(rv.encode('cp932','ignore').decode('cp932'))
 		except UnicodeError as e:
 			print("Unicode error", e)
 
-#		print(num, '\t', menu, '\t', score, '\t', noodle_type, '\t', soup_type, '\t', shop_id, '\t', user_id, '\t', likes, '\t', review_timestamp[:10])
+#		print(num, '\t', menu, '\t', score, '\t', category, '\t', _type, '\t', soup_type, '\t', shop_id, '\t', user_id, '\t', likes, '\t', review_timestamp[:10])
 	except:
 		sys.stderr.write ('error, num=' + format(num) + '\n')
 		pass
