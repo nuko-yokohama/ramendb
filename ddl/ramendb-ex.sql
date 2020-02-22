6f0b0bd2f653f6c14882ba50484ed8eb2cedc0dc
nuko-yokohama <nuko.yokohama@gmail.com>
2020-02-22 22:56:01 +0000

    神奈川家系ビューの修正（町田市を追加、tags 判定の修正） 店舗＆レビュー情報の追加

--- ddl/ramendb-ex.sql
+++ ddl/ramendb-ex.sql
@@ -6,9 +6,11 @@
 --
 -- 神奈川の家系レビュー(IMV)
 -- 実験的にこのビューをIMVで作成する。このビューに対するORDER BY LIMITがそのままだとイケてないので、reg_date に改めてbtreeを設定したほうが良さげ。
+-- 町田を検索対象に追加。町田は神奈川。
 --
 CREATE INCREMENTAL MATERIALIZED VIEW kanagawa_house_review_imv AS
 SELECT s.sid,
+    s.area,
     s.name,
     s.branch,
     r.menu,
@@ -16,7 +18,7 @@ SELECT s.sid,
     r.reg_date
    FROM shops s
      JOIN reviews r ON s.sid = r.sid
-  WHERE s.pref = '神奈川県' AND s.status = 'open' AND s.tags = '{家系}'
+  WHERE (s.pref = '神奈川県'::text OR s.area = '町田市'::text) AND s.status = 'open'::text AND s.tags @> '{家系}'::text[]
 ;
 
 --
@@ -24,6 +26,7 @@ SELECT s.sid,
 --
 CREATE VIEW kanagawa_house_review_v AS
 SELECT s.sid,
+    s.area,
     s.name,
     s.branch,
     r.menu,
@@ -31,7 +34,7 @@ SELECT s.sid,
     r.reg_date
    FROM shops s
      JOIN reviews r ON s.sid = r.sid
-  WHERE s.pref = '神奈川県' AND s.status = 'open' AND s.tags = '{家系}'
+  WHERE (s.pref = '神奈川県'::text OR s.area = '町田市'::text) AND s.status = 'open'::text AND s.tags @> '{家系}'::text[]
 ;
 
 --
