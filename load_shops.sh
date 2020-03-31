923d7144a11bbdc262e0fb9ada7f68ef42c0fed6
nuko-yokohama <nuko.yokohama@gmail.com>
2020-03-31 21:22:05 +0000

    店舗およびレビューの収集＆ロード用シェルを追加。それを使って店舗＆レビューの追加

---
+++ load_shops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "get shops data" $1 $2
+python3 get_rdb_shop.py $1 $2 > /tmp/s.txt
+
+# load shops daat
+echo "load shops data" $1 $2
+psql -U postgres ramendb -e -c "DELETE FROM shops WHERE sid >= $1"
+psql -U postgres ramendb -e  -c "COPY shops FROM '/tmp/s.txt'"
+psql -U postgres ramendb -e -c "CLUSTER shops"
+psql -U postgres ramendb -e -c "TABLE aggs_v"
+
