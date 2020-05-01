7b24677a1beae1395fe604a655e66ad3091a0090
nuko-yokohama <nuko.yokohama@gmail.com>
2020-05-01 01:49:33 +0000

    ユーザ情報収集＆ロードシェルの追加

---
+++ load_users.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "get shops data" $1 $2
+python3 get_rdb_user.py $1 $2 > /tmp/u.txt
+
+# load shops daat
+echo "load users data" $1 $2
+psql -U postgres ramendb -e -c "DELETE FROM users WHERE uid >= $1"
+psql -U postgres ramendb -e  -c "COPY users FROM '/tmp/u.txt'"
+psql -U postgres ramendb -e -c "CLUSTER users"
+psql -U postgres ramendb -e -c "TABLE aggs_v"
+
