d57b4d58598bf46fb1482009d4448661e7918b4e
nuko-yokohama <nuko.yokohama@gmail.com>
2021-12-12 09:35:32 +0900

    データ再ロード用SQLファイルの追加

---
+++ data/load.sql
@@ -0,0 +1,7 @@
+-- psql用ロードスクリプト
+\copy shops FROM shops.txt
+\copy shops_location FROM shops_location.txt
+\copy reviews FROM reviews_0000001_1000000.txt
+\copy reviews FROM reviews.txt
+\copy users FROM users.txt
+
